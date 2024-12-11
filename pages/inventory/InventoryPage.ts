6f08e1dbc81fcf0b98edb3cbc041cb562e30f1f7
edvard1998 <e.soghomonyan98@gmail.com>
2024-12-11 12:46:42 +0400

    Created CardView page

--- pages/inventory/InventoryPage.ts
+++ pages/inventory/InventoryPage.ts
@@ -1,8 +1,16 @@
-import { Page } from "@playwright/test";
+import { Locator, Page, expect } from "@playwright/test";
 import { PageObject } from "../PageObject";
 
 export class InventoryPage extends PageObject {
+    private readonly cardList: Locator;
+    
     constructor(page: Page) {
         super(page, 'v1/inventory.html');
+
+        this.cardList = this.page.locator('//div[@class="inventory_list"]');
+    }
+
+    async addToCard() {
+        await expect(this.cardList).toBeVisible();
     }
 }
